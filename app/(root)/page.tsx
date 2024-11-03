36bf813b3d6df10642a554ddaf645e42f3cb6331
jokovivanco <jokovivanco@gmail.com>
2024-11-03 08:31:19 +0800

    ESLint & Prettier Setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,5 @@
 const Page = () => {
-  return <h1 className="text-3xl font-bold">Tested Text</h1>;
+  return <h1 className="text-3xl font-bold text-white">Tested Text</h1>;
 };
 
 export default Page;
